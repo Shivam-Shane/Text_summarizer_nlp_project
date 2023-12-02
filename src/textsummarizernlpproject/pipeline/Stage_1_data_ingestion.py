69ed94f736a21102ffdc2c28e07781dc84fd31ba
Shanezatch <sk0551460@gamil.com>
2023-12-02 11:19:48 +0530

    Fix typo error at class name

--- src/textsummarizernlpproject/pipeline/Stage_1_data_ingestion.py
+++ src/textsummarizernlpproject/pipeline/Stage_1_data_ingestion.py
@@ -1,14 +1,14 @@
 from logger import logging
 from src.textsummarizernlpproject.config.configuration import ConfigurationManager
-from src.textsummarizernlpproject.conponents.data_ingestion import DataIngetsion
+from src.textsummarizernlpproject.conponents.data_ingestion import DataIngestion
 
 class DataIngestionTrainingPipeline:
     def __init__(self):
         pass
     def main(self):
-        config=ConfigurationManager()
-        dataingestionconfig=config.get_data_ingestion_config()
-        dataingestion=DataIngetsion(config=dataingestionconfig)
+        config=ConfigurationManager() ## To access the configuration methods
+        dataingestionconfig=config.get_data_ingestion_config() ## To access the data_ingestion config
+        dataingestion=DataIngestion(config=dataingestionconfig)
         dataingestion.download_file()
         dataingestion.extract_data()
 

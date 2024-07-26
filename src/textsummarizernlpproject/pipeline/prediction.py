bdd67f467be907f0728118df8350bc645143bd93
Shivam-Shane <sk0551460@gmail.com>
2024-07-27 01:28:42 +0530

    Added precheck and preloading of model files so it won't get loaded everytime we run it

--- src/textsummarizernlpproject/pipeline/prediction.py
+++ src/textsummarizernlpproject/pipeline/prediction.py
@@ -3,8 +3,36 @@ from src.textsummarizernlpproject.config.configuration import ConfigurationManag
 from transformers import pipeline,AutoTokenizer
 
 class PredictPipelineClass:
+
+    # class variables
+    _runonce = False
+    _tokenizer = None
+    _gen_kwargs = None
+    _pipeline=None
     def __init__(self):
         self.config= ConfigurationManager().get_model_evaluation_config()
+        if not PredictPipelineClass._runonce: # making sure we don't call this twice reducing workload time
+            try:
+                PredictPipelineClass._tokenizer=AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+                # tokenizer
+                PredictPipelineClass._pipeline=pipeline("summarization",model=str(self.config.model_name),tokenizer=PredictPipelineClass._tokenizer)
+                # pipeline
+                PredictPipelineClass._gen_kwargs={'length_penalty':1.8,  # less than 1.0 encourages generation of shorter summaries.
+                        'num_beams':10, #beams improves quality of summary considering potential sequences
+                        "max_length":500, # max length of summary
+                        'min_length': 100, # min length of summary
+                        'no_repeat_ngram_size': 3, # no repeat of ngram size in summary
+                        } 
+                PredictPipelineClass._runonce = True
+                logging.info(f"Model and tokenizer loaded successfully")
+            except Exception as e:
+                 logging.error(f"Error initializing PredictPipelineClass: {e}")
+                 raise e
+        self.pipeline = PredictPipelineClass._pipeline
+        self.tokenizer = PredictPipelineClass._tokenizer
+        self.gen_kwargs = PredictPipelineClass._gen_kwargs
+        logging.info(f"Predict pipeline class initialized successfully")
+
     def predict(self,text):
             """
             Summarize the data and return
@@ -13,11 +41,14 @@ class PredictPipelineClass:
             Returns:
                 text: string(Summarized text)    
             """
-            logging.info(f">>>> Inside {self.__class__.__name__}.{self.__init__.__name__}")
+            logging.info(f">>>> Inside {self.__class__.__name__}.{self.predict.__name__}")
             logging.info(f"Starting prediction")
-            tokenizer=AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-            gen_kwags={'length_penalty':0.8,'num_beams':10,"max_length":500}
-            pipe=pipeline("summarization",model=str(self.config.model_name),tokenizer=tokenizer)
-            output=pipe(text,**gen_kwags)[0]["summary_text"].replace("<n>", " ") #type: ignore
-            logging.info("Returing result")
-            return output
+            try:
+                 
+                output=self.pipeline(text,**self.gen_kwargs)[0]["summary_text"].replace("<n>", " ") #type: ignore
+                logging.info("Returning result")
+                return output
+            except Exception as e:
+                 logging.error(f"Error in predicting: {e}")
+                 raise e
+# ----------------------------------------------------------------------------------------------------------------
